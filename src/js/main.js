77645b40b81f0773bc400ce88da151ff4a54826a
MrHidalgo <Luckyhidalgo@gmail.com>
2017-01-26 13:55:25 +0200

    All block [!responsive]

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         $(this).toggleClass("active");
     });
 
+
     /* SLIDER */
     $(".slider__carousel").owlCarousel({
         items:1,
@@ -15,6 +16,35 @@ $(document).ready(function() {
     });
     var count = 1;
     $(".slider__carousel .owl-dot").each(function(){
+        $(this).find("span").text("0" + count);
+        count++;
+    });
+
+
+    /* DEVICE BTN */
+    $(".device__btn").on("click", function(e) {
+        e.preventDefault();
+
+        $('.device__btn').removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".device__carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: true
+    });
+
+
+    /* TESTIMONIALS */
+    $(".testimonials__carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: true
+    });
+    var count = 1;
+    $(".testimonials__carousel .owl-dot").each(function(){
         $(this).find("span").text("0" + count);
         count++;
     });
