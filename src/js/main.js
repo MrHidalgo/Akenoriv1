91a8adec39c37b3dfae3684ce84834cf9bfda699
MrHidalgo <Luckyhidalgo@gmail.com>
2017-02-06 13:54:11 +0200

    Add device slider - new image Fix nav CheckViewport Smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -59,11 +59,26 @@ $(document).ready(function() {
 
 
     /* DEVICE BTN */
-    $(".device__btn").on("click", function(e) {
-        e.preventDefault();
+    $(".device__btn").on("click", function() {
+        var linkAttr = $(this).attr("data-more");
 
-        $('.device__btn').removeClass("active");
+        $(".device__btn").removeClass("active");
         $(this).addClass("active");
+
+        $(".device__carousel").css(
+            {
+                opacity: 0,
+                height: 0,
+                visibility: "hidden"
+            }
+        );
+        $(".device__carousel-" + linkAttr).css(
+            {
+                opacity: 1,
+                height: "auto",
+                visibility: "visible"
+            }
+        );
     });
     $(".device__carousel").owlCarousel({
         items: 1,
@@ -87,6 +102,65 @@ $(document).ready(function() {
     });
 
 
+    /* BTN HEADER ARROW */
+    $(".btn-arrow").hover(function() {
+        $(this).toggleClass("animated pulse infinite");
+    });
+
+
+    /* SMOOTH SCROLL */
+    $(".nav, .header__arrow").on("click", "a", function (e) {
+        e.preventDefault();
+
+        var id          = $(this).attr('href'),
+            navHeight   = $(".header__fixed").outerHeight();
+
+        $(".btn-menu_js").removeClass("active");
+        $(".nav").slideUp(300);
+        $("body").removeClass("open-menu");
+
+        if(id === "#top") {
+            $('body, html').animate({
+                scrollTop: 0
+            }, 1000);
+        } else {
+            var top;
+            if($(window).width() > '1024') {
+                top = $(id).offset().top - navHeight;
+            } else {
+                top = $(id).offset().top;
+            }
+
+            $('body, html').animate({
+                scrollTop: top
+            }, 1000);
+        }
+    });
+
+
+    /* MODAL VIDEO */
+    $(document).on("keyup", function(e) {
+        if (e.keyCode == 27) {
+            $("body").removeClass("open-popup");
+            $(".modal").fadeOut(300);
+        }
+    });
+    $(".btn-close").on("click", function(e) {
+        e.preventDefault();
+
+        $("body").removeClass("open-popup");
+        $(".modal").fadeOut(300);
+    });
+    $('.quality__night-js').on("click", function(e) {
+        $("body").addClass("open-popup");
+        $(".modal_night").fadeIn(300);
+    });
+    $('.quality__day-js').on("click", function(e) {
+        $("body").addClass("open-popup");
+        $(".modal_day").fadeIn(300);
+    });
+
+
     /* ANIMATION - VIEW PORT CHECK PAGE */
     if($(window).width() > '767') {
         var classNameSection    =   ".main__title, .main__list, .difference__img, " +
@@ -98,7 +172,23 @@ $(document).ready(function() {
 
         $(classNameSection).addClass('hidden').viewportChecker({
                 classToAdd: 'visible animated slideInUp',
-                classToRemove : 'hidden'
+                classToRemove : 'hidden',
+                removeClassAfterAnimation: true
+            }
+        );
+
+        $(".header__img-shadow").addClass('hidden').viewportChecker({
+                classToAdd: 'visible active',
+                classToRemove : 'hidden',
+                offset: -100,
+                removeClassAfterAnimation: true
+            }
+        );
+        $(".header__img-register").addClass('hidden').viewportChecker({
+                classToAdd: 'visible active',
+                classToRemove : 'hidden',
+                offset: -100,
+                removeClassAfterAnimation: true
             }
         );
     }
